4f0dcec49e4db6690a73837c0e50273c1d4b26d8
Rananjaya <rananjaya@tsdclanka.com>
2020-10-18 15:03:06 +0530

    mirro raw

--- lib/idention.ex
+++ lib/idention.ex
@@ -4,6 +4,7 @@ defmodule Idention do
      input
      |> hash_input
      |> pick_color
+     |> build_grid
   end
 def pick_color( %Idention.Image{hex: [r,g,b | _tail]} = image) do 
   #this is the pattern maching
@@ -12,6 +13,21 @@ def pick_color( %Idention.Image{hex: [r,g,b | _tail]} = image) do
 %Idention.Image{image | color: {r,g,b}}
   
 end
+
+def build_grid(%Idention.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3)
+    
+
+end
+
+def mirror_row(row) do
+  [first, second | _tail] = row
+
+  row ++ [second,first]
+
+end
+
 
   def hash_input(input) do
     hex = :crypto.hash(:md5,input)
