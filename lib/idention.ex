dec809baa9d8499c01baed2ca4a4509ea117c010
Rananjaya <rananjaya@tsdclanka.com>
2020-10-18 16:07:04 +0530

    add new property to struct and update the struct as map

--- lib/idention.ex
+++ lib/idention.ex
@@ -15,11 +15,14 @@ def pick_color( %Idention.Image{hex: [r,g,b | _tail]} = image) do
 end
 
 def build_grid(%Idention.Image{hex: hex} = image) do
-    hex
+
+    grid = hex
     |> Enum.chunk(3)
     |> Enum.map(&mirror_row/1)
     |> List.flatten
     |> Enum.with_index
+
+    %Idention.Image{image | grid: grid}
   
   end
 
--- lib/image.ex
+++ lib/image.ex
@@ -1,4 +1,4 @@
 defmodule Idention.Image do #create new module
     #purpose pf this is hod all our structs
-    defstruct hex: nil, color: nil
+    defstruct hex: nil, color: nil, grid: nil
 end
